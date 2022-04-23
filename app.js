dce179883fd15206f83857ed55869fd520bbf446
Mihir <savla.mp@somaiya.edu>
2022-04-24 03:01:07 +0530

    lot of stufff on inserting data

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 var MongoClient = require("mongodb").MongoClient;
 var url = require("url");
 var urldb = "mongodb://localhost:27017/";
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const internal = require("stream");
 
 //setting up ejs
 app.set("view engine", "ejs");
@@ -10,10 +13,39 @@ app.set("view engine", "ejs");
 //setting up templates folder for storing css
 app.use(express.static("templates"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect(urldb, { useNewUrlParser: true }, { useUnifiedTopology: true }).then(() => {
+    console.log("conneted successfully");
+}).catch((err) => {
+    console.log(err);
+});
+const dataSchema = {
+    Name: String,
+    City: String,
+    State: String,
+    Phone_no: String,
+    blood_type: String
+}
+
+const data = mongoose.model("mydb", dataSchema);
+
 //routing home page
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.post("/", function(req, res) {
+    let newNote = new data({
+        Name: req.body.Name,
+        State: req.body.State,
+        City: req.body.City,
+        Phone_no: req.body.Phone_no,
+        Blood_type: req.body.Blood_type
+    });
+    console.log(newNote);
+    newNote.save().then(() => console.log("saved succesfully"));
+    res.redirect('/');
+})
 
 //routing search page
 app.get("/search", (req, res) => {
@@ -32,8 +64,8 @@ app.get("/search", (req, res) => {
             dbo.collection("donor_info").find(que).toArray(function(err, result) {
                 if (err) throw err;
 
-                //passing all the filtered queries to our html page in the form of variable "data"
-                res.render("search", { data: result });
+                //passing all the filtered queries to our html page in the form of variable "array"
+                res.render("search", { values: result });
                 db.close();
             });
         });
@@ -41,7 +73,7 @@ app.get("/search", (req, res) => {
 
     //if there is no quert, then render as it is 
     else {
-        res.render("search", { data: " " });
+        res.render("search", { values: " " });
     }
 });
 
