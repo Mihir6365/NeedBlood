0d5471d68e23f9c646e0680a9eccded6c5f5ba73
Mihir <savla.mp@somaiya.edu>
2022-04-23 18:24:01 +0530

    a lot of chanegs

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+const express = require("express");
+const app = express();
+var MongoClient = require("mongodb").MongoClient;
+var url = require("url");
+var urldb = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
-    var query = { blood_type: "O+" };
-    dbo.collection("donor_info").find(query).toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-        db.close();
-    });
+app.set("view engine", "ejs");
+
+//save all css files in templates folder
+
+app.use(express.static("templates"));
+app.get("/", (req, res) => {
+    res.render("home");
 });
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/search", (req, res) => {
+    var q = url.parse(req.url, true);
+    var query = q.query;
+
+    if (query.city && query.blood_type) {
+        console.log("true");
+        MongoClient.connect(urldb, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("mydb");
+            var que = { blood_type: query.blood_type, city: query.city };
+            dbo.collection("donor_info").find(que).toArray(function(err, result) {
+                if (err) throw err;
+                res.render("search", { variable: result });
+                console.log(result);
+
+                db.close();
+            });
+        });
+    } else {
+        res.render("search", { variable: " " });
+        console.log("false");
+    }
+});
+
+app.listen(3000, () => console.log("Server Started"));
--- database.js
+++ database.js
@@ -4,9 +4,10 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
-    dbo.collection("donor_info").findOne({}, function(err, result) {
+    var myobj = { name: "Donor7", address: "Mah", phone_no: "6543219875", blood_type: "A+", city: "Thane" }
+    dbo.collection("donor_info").insertOne(myobj, function(err, res) {
         if (err) throw err;
-        console.log(result.name);
+        console.log("1 document inserted");
         db.close();
     });
 });
